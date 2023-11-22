b432571ca9109d9e9911888fda7964b534477ab1
giuliano iantomasi <giulianoiantomasi@gmail.com>
2023-11-22 17:10:13 -0500

    Initial Commit

---
+++ GiuliettaInventoryManagement/GiuliettaInventoryManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiuliettaInventoryManagementApp.swift
+//  GiuliettaInventoryManagement
+//
+//  Created by ella iantomasi on 2023-11-22.
+//
+
+import SwiftUI
+
+@main
+struct GiuliettaInventoryManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
